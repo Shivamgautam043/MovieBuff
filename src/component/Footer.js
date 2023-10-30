361ae8ffffa5bcc70dba66b0eb98fe0a6ade7f1a
shivam <gautamshivam043@gmail.com>
2023-10-31 01:33:51 +0530

    MovieBuff

---
+++ src/component/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <h2 className="footer-text">
+          Made with <span className="heart">❤️</span> by SHIVAM
+        </h2>
+        <p className="footer-contact">
+          Contact: gautamshivam043@gmail.com | Phone: 9354750844
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
